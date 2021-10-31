f78e6ec0282bc6e23612987ad08391c9037a6429
zahangir710 <zahangir710@gmail.com>
2021-11-01 00:59:11 +0100

    db user updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port=process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASS}@cluster0.dyxrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dyxrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
